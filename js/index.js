ddb269d94ab4232423013ca1a576fd9bb4151179
Utkarsh Shresth <79664473+Shresthutk@users.noreply.github.com>
2021-08-13 18:35:09 +0530

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,15 +1,15 @@
 console.log('js file included');
-let obj='';
+let obj = '';
 fetch('https://api.adviceslip.com/advice')
-.then(res=>res.json())
-.then(data=>obj=data);
-let advice=document.getElementById('advice');
+.then(res => res.json())
+.then(data => obj = data);
+let advice = document.getElementById('advice');
+advice.innerHTML='';
 setTimeout(() => {
-    var displayAdvc=obj.slip.advice;
-    advice.innerHTML=displayAdvc;
-}, 1800);
-let btn=document.getElementById('btn');
-btn.addEventListener('click',troll);
-function troll(){
-    advice.innerHTML=` <img src="./img/trollFace.gif" alt="trollFace" srcset="">`
-}
+    var displayAdvc = obj.slip.advice;
+    advice.innerHTML = displayAdvc;
+}, 1500);
+let btn = document.getElementById('btn');
+btn.addEventListener('click', () => {
+    advice.innerHTML = ` <img src="./img/trollFace.gif" alt="trollFace" srcset="">`
+});
